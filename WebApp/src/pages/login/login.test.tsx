6f80a884c37c0a6b538d13de0506e60405be0c22
Shubham Khuva <tech@shubhamkhuva.com>
2024-03-26 17:12:04 +0400

    testing removed

--- WebApp/src/pages/login/login.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Login from "./";
-
-test("renders welcome message", () => {
-  render(<Login />);
-  const welcomeElement = screen.getByText(/Welcome Back/i);
-  expect(welcomeElement).toBeInTheDocument();
-});
