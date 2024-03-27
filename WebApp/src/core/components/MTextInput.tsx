a60449c9b53b950f4a053eb3e1b9c45bdd37b2ec
Shubham Khuva <tech@shubhamkhuva.com>
2024-03-27 20:36:46 +0400

    Screenshot Updated

--- WebApp/src/core/components/MTextInput.tsx
+++ WebApp/src/core/components/MTextInput.tsx
@@ -37,7 +37,7 @@ const MTextInput = (props: MTextInputProps) => {
             onBlur={onBlur}
             value={value}
             type={isPassword ? "password" : "text"}
-            InputLabelProps={{ shrink: true, required: required }}
+            InputLabelProps={{ required: required }}
             variant="outlined"
             style={{ width: "100%", ...style }}
             error={Boolean(error)}
