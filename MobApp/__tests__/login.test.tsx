ab7d58d17f4ee524f2642753e14f33f4afcae42f
Shubham Khuva <tech@shubhamkhuva.com>
2024-03-27 16:43:33 +0400

    test cases changes

--- MobApp/__tests__/login.test.tsx
+++ MobApp/__tests__/login.test.tsx
@@ -1,21 +1,25 @@
 import renderer, {act} from 'react-test-renderer';
 import React from 'react';
 import Login from '../src/screens/Login';
-import '@testing-library/jest-dom'; // You might need to import this if you're using testing-library utilities
+import '@testing-library/jest-dom';
 import {Provider} from 'react-redux';
 import store from '../src/context/store';
 import {render, fireEvent, waitFor} from '@testing-library/react-native';
 
-describe('Login component', () => {
-  test('renders correctly', async () => {
-    await act(async () => {
-      const {getByText, getByPlaceholderText} = render(
-        <Provider store={store}>
-          <Login />
-        </Provider>,
-      );
+jest.mock('@react-native-async-storage/async-storage', () => ({
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+}));
 
-      expect(getByText('Login')).toBeTruthy();
-    });
+describe('Login component', () => {
+  test('validates form inputs', () => {
+    const {getByTestId} = render(<Login />);
+    const usernameInput = getByTestId('username-input');
+    const passwordInput = getByTestId('password-input');
+
+    fireEvent.changeText(usernameInput, '');
+    fireEvent.changeText(passwordInput, '');
+
+    // Add assertions to check if validation messages are displayed
   });
 });
